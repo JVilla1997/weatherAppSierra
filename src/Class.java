e1705ef17da96673e0f6a586d9dc7e360c5649c3
Daniel Lundy <dlundy1@sstudent.sierracollege.edu>
2018-04-18 15:05:38 -0700

    Prototype

--- src/Class.java
+++ src/Class.java
@@ -1,2 +1,92 @@
-public class Class {
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+public class Class
+{
+    String zipcode;
+    final String APIKEY = "1655f919bbcd29ed";
+    JsonElement jse;
+
+    public Class(String location)
+    {
+        zipcode = location;
+    }
+
+    public String getCity(String key) {
+        if (jse == null) fetch();
+        return jse.getAsJsonObject().get("current_observation").getAsJsonObject().get("display_location").getAsJsonObject().get(key).getAsString();
+    }
+
+    public String getLoc()
+    {
+        return getCity("city");
+    }
+
+    public String getData(String key) {
+        if (jse == null) fetch();
+        return jse.getAsJsonObject().get("current_observation").getAsJsonObject().get(key).getAsString();
+    }
+
+    public String getTemp()
+    {
+        return getData("temp_f");
+    }
+
+    public String getConditions()
+    {
+        return getData("weather");
+    }
+
+    public String getWind()
+    {
+        return getData("wind_string");
+    }
+
+    public String getIcon()
+    {
+        return getData("icon_url");
+    }
+
+    public void fetch()
+    {
+        String weatherRequest = "http://api.wunderground.com/api/" +
+                APIKEY + "/conditions/q/" + zipcode + ".json";
+
+        try
+        {
+            URL weatherURL = new URL(weatherRequest);
+
+            InputStream is = weatherURL.openStream();
+            InputStreamReader isr = new InputStreamReader(is);
+            BufferedReader br = new BufferedReader(isr);
+
+            jse = new JsonParser().parse(br);
+
+        }
+        catch (java.net.MalformedURLException mue)
+        {
+            System.out.println("Zipcode not well formed");
+            mue.printStackTrace();
+        }
+        catch (java.io.IOException ioe)
+        {
+            System.out.println("Got IO Exception");
+            ioe.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        Class b = new Class("http://api.wunderground.com/api/1655f919bbcd29ed/conditions/q/95843.json");
+        System.out.println(b.getTemp());
+        System.out.println(b.getConditions());
+        System.out.println(b.getWind());
+        System.out.println(b.getIcon());
+        System.out.println(b.getLoc());
+    }
 }
+
--- src/GUI.java
+++ src/GUI.java
@@ -1,2 +1,98 @@
-public class GUI {
+import acm.graphics.GCanvas;
+import acm.graphics.GImage;
+import acm.program.Program;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+
+
+
+public class GUI extends Program
+{
+    private JTextField zipField;
+    private JTextField cityField;
+    private JTextField tempField;
+    private JTextField conditionField;
+    private GImage gIcon;
+
+    public GUI()
+    {
+        start();
+        setSize(390, 358);
+    }
+    // testing 123
+    public void init()
+    {
+        GCanvas canvas = new GCanvas();
+        add(canvas);
+        //add image
+        GImage Fair_Weather = new GImage("images/Fair_Weather.jpg");
+        canvas.add(Fair_Weather,0 ,0 );
+
+
+        JLabel zipLabel = new JLabel("Zip");
+        JLabel cityLabel = new JLabel("City");
+        JLabel tempLabel = new JLabel("Temp");
+        JLabel conditionLabel = new JLabel("Condition");
+
+
+        zipField = new JTextField();
+        cityField = new JTextField();
+        tempField = new JTextField();
+        conditionField = new JTextField();
+        gIcon = new GImage("");
+
+
+        JButton goButton = new JButton("Go");
+        JButton clearButton = new JButton("Clear");
+
+        canvas.add(zipLabel, 20, 20);
+        canvas.add(cityLabel, 20, 70);
+        canvas.add(tempLabel, 20, 120);
+        canvas.add(conditionLabel, 20, 170);
+        canvas.add(gIcon, 250, 145);
+
+        canvas.add(zipField, 80, 20);
+        canvas.add(cityField, 80, 70);
+        canvas.add(tempField, 80, 120);
+        canvas.add(conditionField, 80, 170);
+        zipField.setSize(150, 20);
+        cityField.setSize(150, 20);
+        tempField.setSize(150, 20);
+        conditionField.setSize(150, 20);
+
+
+        canvas.add(goButton, 60,220);
+        canvas.add(clearButton, 160, 220);
+
+        addActionListeners();
+    }
+    ///comment
+    public void actionPerformed(ActionEvent e)
+    {
+        String what = e.getActionCommand();
+
+        if (what.equals("Go"))
+        {
+            Class b = new Class(zipField.getText());
+            cityField.setText(b.getLoc());
+            tempField.setText(b.getTemp());
+            conditionField.setText(b.getConditions());
+            gIcon.setImage(b.getIcon());
+
+        }
+        else if (what.equals("Clear"))
+        {
+            zipField.setText("");
+            cityField.setText("");
+            tempField.setText("");
+            conditionField.setText("");
+            gIcon.setImage("");
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        GUI g = new GUI();
+    }
 }
